3cc22412dfb2860fe97896dec81b13883dd89790
Nuwin Tharana <134575245+NUWIN5@users.noreply.github.com>
2024-06-10 17:31:13 +0530

    change the table view

--- resources/views/products/index.blade.php
+++ resources/views/products/index.blade.php
@@ -20,35 +20,39 @@
     </div>
 
     <div>
-        <table border="1">
-            <tr>
-                <th>Product Id</th>
-                <th>Name</th>
-                <th>Quantity</th>
-                <th>Price</th>
-                <th>Edit</th>
-                <th>Delete</th>
-                
-            </tr>
-            @foreach($products as $product)
-            <tr>
-                <td>{{$product->id}}</td>
-                <td>{{$product->name}}</td>
-                <td>{{$product->quantity}}</td>
-                <td>{{$product->price}}</td>
-                <td>
-                    <a href="{{route('product.edit', ['product'=> $product])}}">Edit</a>
-                </td>
-                <td>
-                    <form method="post" action="{{route('product.delete', ['product'=> $product])}}">
-                        @csrf
-                        @method('delete')
-                        <button type="submit">Delete</button>
-                    </form>
-                </td>
+    <table border="1" class="large-table">
+    <thead>
+        <tr>
+            <th>Product Id</th>
+            <th>Name</th>
+            <th>Quantity</th>
+            <th>Price</th>
+            <th>Edit</th>
+            <th>Delete</th>
+        </tr>
+    </thead>
+    <tbody>
+        @foreach($products as $product)
+        <tr>
+            <td>{{ $product->id }}</td>
+            <td>{{ $product->name }}</td>
+            <td>{{ $product->quantity }}</td>
+            <td>{{ $product->price }}</td>
+            <td>
+                <a href="{{ route('product.edit', ['product'=> $product]) }}">Edit</a>
+            </td>
+            <td>
+                <form method="post" action="{{ route('product.delete', ['product'=> $product]) }}">
+                    @csrf
+                    @method('delete')
+                    <button type="submit">Delete</button>
+                </form>
+            </td>
+        </tr>
+        @endforeach
+    </tbody>
+</table>
 
-            @endforeach
-        </table>
     </div>
     
 </body>
