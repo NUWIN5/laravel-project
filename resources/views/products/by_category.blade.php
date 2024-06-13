1d76116f7eb92e21d258029cb85788abba7ae63d
Nuwin Tharana <134575245+NUWIN5@users.noreply.github.com>
2024-06-13 13:20:15 +0530

    create a new view to show products in a product category

---
+++ resources/views/products/by_category.blade.php
@@ -0,0 +1,46 @@
+<!-- resources/views/products/by_category.blade.php -->
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Products in {{ $category->name }}</title>
+</head>
+<body>
+    <h1>Products in {{ $category->name }}</h1>
+    <a href="{{ route('products.create') }}">Add New Product</a>
+    @if(session('success'))
+        <div>{{ session('success') }}</div>
+    @endif
+    <table>
+        <thead>
+            <tr>
+                <th>ID</th>
+                <th>Name</th>
+                <th>Quantity</th>
+                <th>Price</th>
+                <th>Actions</th>
+            </tr>
+        </thead>
+        <tbody>
+            @foreach($products as $product)
+                <tr>
+                    <td>{{ $product->id }}</td>
+                    <td>{{ $product->name }}</td>
+                    <td>{{ $product->quantity }}</td>
+                    <td>{{ $product->price }}</td>
+                    <td>
+                        <a href="{{ route('products.edit', $product->id) }}">Edit</a>
+                        <form action="{{ route('products.destroy', $product->id) }}" method="POST" style="display:inline;">
+                            @csrf
+                            @method('DELETE')
+                            <button type="submit">Delete</button>
+                        </form>
+                    </td>
+                </tr>
+            @endforeach
+        </tbody>
+    </table>
+</body>
+</html>
